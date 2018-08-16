8fe4ba27963fd3a712e49e4cb0ff0fadf9054a4f
wereboss <tellsrinath@gmail.com>
2018-08-16 22:05:01 +0800

    Approvals Base skeleton working version

---
+++ loadjson.js
@@ -0,0 +1,22 @@
+//JSON loading functions
+function loadJSON(url) {
+    var tmpObj = [];
+    var waitB = true;
+    var waitI;
+    //console.log("Inside loadJSON with url " + url);
+    $.ajaxSetup({
+        async: false
+    });
+    $.getJSON(url, function (data) {
+        //console.log("StringiFy JSON Data" + JSON.stringify(data.d.results));
+        $.each(data.d.results, function (ini, it) {
+            tmpObj.push(it);
+        });
+        //console.log("StringiFy tmpObj" + JSON.stringify(tmpObj));
+        //console.log("tmpObj len " + tmpObj.length);
+    });
+    $.ajaxSetup({
+        async: true
+    });
+    return tmpObj;
+}
